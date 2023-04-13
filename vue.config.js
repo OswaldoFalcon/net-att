d90d16102700e754cc3b191bed377a1581c4d909
OswaldoFalcon <77717622+OswaldoFalcon@users.noreply.github.com>
2023-04-13 16:22:30 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,1 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-    devServer: {
-      proxy: {
-        '/tasks': {
-          target: 'https://employee-atendance-node-production.up.railway.app/',
-          timeout: 6000,
-        secure: false,
-        changeOrigin: true
-        },
-      },
-    },
-  },
-})
+
